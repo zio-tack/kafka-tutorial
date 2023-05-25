1368c5837bd43de2edcdab7eecd07fa7f2902775
francesco-bruni <f.bruni@mediatech.it>
2023-05-25 12:31:20 +0200

    KAFKA TUTORIAL
    constants from properties

--- src/main/java/it/mediatech/kafka/kafka/KafkaConsumer.java
+++ src/main/java/it/mediatech/kafka/kafka/KafkaConsumer.java
@@ -10,7 +10,7 @@ public class KafkaConsumer {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(KafkaConsumer.class);
 
-    @KafkaListener(topics = {"${spring.kafka.topic.name}"}, groupId = "myConsumerGroup")
+    @KafkaListener(topics = {"${spring.kafka.topic.name}"}, groupId = "${spring.kafka.consumer.group.id}")
     public void consume(String message) {
         LOGGER.info(String.format("Message received -> %s", message));
     }
